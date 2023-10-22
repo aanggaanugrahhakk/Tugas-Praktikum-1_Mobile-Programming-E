905b115517d11864a511a80a93faa4dfe7abf571
aanggaanugrahhakk <92570666+aanggaanugrahhakk@users.noreply.github.com>
2023-10-22 11:59:06 +0700

    Update 202131037_Anugrah AK._Widget.dart

--- 202131037_anugrah_ak_widget/lib/202131037_Anugrah AK._Widget.dart
+++ 202131037_anugrah_ak_widget/lib/202131037_Anugrah AK._Widget.dart
@@ -16,22 +16,22 @@ class MyApp extends StatelessWidget {
           body: Column(children: <Widget>[
             Text(
               'Nama: Anugrah AK.',
-              style: GoogleFonts.aboreto(fontSize: 35, height: 2.0),
+              style: GoogleFonts.aboreto(fontSize: 15, height: 2.0),
             ),
-            Image.network('https://s2.dmcdn.net/v/UY3_l1Zk5pLlb7iGS/x240'),
             Text(
               'Nama Panggilan: Angga',
-              style: GoogleFonts.abrilFatface(height: 2.0, fontSize: 35),
+              style: GoogleFonts.abrilFatface(height: 2.0, fontSize: 15),
             ),
-            Image.network('https://s2.dmcdn.net/v/Sw2il1WVl7mUFXVko/x240'),
             Text(
               'Asal: Desa Lagego, Kec. Burau, Kab. Luwu Timur, Sulawesi Selatan',
-              style: GoogleFonts.abhayaLibre(fontSize: 35, height: 2.0),
+              style: GoogleFonts.abhayaLibre(fontSize: 15, height: 2.0),
             ),
             Text(
               'Anugrah AK. - 202131037 - Mobile Programming E',
-              style: GoogleFonts.abyssinicaSil(height: 2.0, fontSize: 40),
+              style: GoogleFonts.abyssinicaSil(height: 2.0, fontSize: 20),
             ),
+            Image.network(
+                'https://cdn.pixabay.com/photo/2023/09/25/10/05/ai-generated-8274619_1280.png'),
           ]),
         ));
   }
